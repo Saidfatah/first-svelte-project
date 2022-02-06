bbc066e13f80344e96727e796c86e7a5264821c2
SaidFatah <said_designer@outlook.com>
2022-02-06 16:53:53 +0100

    created fedBackStore and hoked it in FeedBackList directly using $

--- src/components/FeedBackList.svelte
+++ src/components/FeedBackList.svelte
@@ -3,24 +3,9 @@
   import { FeedBackStore } from "../stores";
   import { fade, slide, scale } from "svelte/transition";
   import FeedBackItem from "./FeedBackItem.svelte";
-
-  let feedback = [];
-
-  let unsbuscribeRef = onMount(() => {
-    unsbuscribeRef = FeedBackStore.subscribe((data) => {
-      console.log(data);
-      feedback = data;
-    });
-    console.log("mounted");
-  });
-
-  onDestroy(() => {
-    console.log("on destroy");
-    unsbuscribeRef();
-  });
 </script>
 
-{#each feedback as fb (fb.id)}
+{#each $FeedBackStore as fb (fb.id)}
   <div in:scale out:fade={{ duration: 500 }}>
     <FeedBackItem item={fb} on:delete-feedback />
   </div>
